a15555c0266e68833b5ee1effd3bede1b1758a12
olgadesatit <desat.olga@web.de>
2024-06-23 22:18:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const button = document.querySelector('button');
 const output = document.querySelector('.output');
 
 button.addEventListener('click', function () {
-    output.innerHTML = 'Mami, i love you💖💖'
+    output.innerHTML = 'Ilter, i love you, my king💖🤴💖'
 })
